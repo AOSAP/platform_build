74eaded32a4980f21057b1cf4cd045443c69e548
Chris Soyars <ctso@ctso.me>
2010-12-23 00:44:33 +0100

    releasetools: squash backuptool support
    this is a squash of the following commits:
    
    commit 30e43f85f8b83dd4210b86363ab912dd640050a0
    Author: Chris Soyars <ctso@ctso.me>
    Date:   Thu Dec 23 00:44:33 2010 +0100
    
        Add otapackage support for backuptool
    
        Change-Id: I512554c579d444067cd3ccbb0e6946a5eb6bc964a
    
        Modular backuptool.sh.  Executes backup and restore methods defined in arbitrary /system/addon.d/*.sh scripts.
    
        * Copy backuptool.functions alongside backuptool.sh.
        * Delete both from /system/bin as they are not useful there.
    
        Patch Series
        ============
        http://review.cyanogenmod.com/#change,13265
        CyanogenMod/android_build
          * edify generator
        http://review.cyanogenmod.com/#change,13266
        CyanogenMod/android_system_core
          * permissions on /system/addon.d
        http://review.cyanogenmod.com/#change,13267
        CyanogenMod/android_vendor_cm
          * 50-cm.sh reference backup script
          * modular backuptool.sh
          * support backuptool.functions used by /system/addon.d/*.sh scripts
    
        Change-Id: I26b4907d28f49c69627699d2accd2f0fa2d1b112
    
        update ota_from_target_files to handle mounting/unmounting for backupscript
    
        backupscript should not be mounting/unmounting itself as it makes other
        scripts have unexpected results (such as modelid_cfg, which expects /system
        to be mounted)
    
        instead have the ota script handle the mounting/unmounting
    
        Change-Id: I94511f4147c624d975cb3ecbeaa8b0e98f63437c
    
        build: Don't run backuptool on GMS builds
    
        Change-Id: I5dde27f9d16b88049171db9805221d92e67f3e5d
    
    commit 66ce73d1ffa81b9dd59639241f2e7172e5485a41
    Author: Tom Marshall <tdm@cyngn.com>
    Date:   Wed Apr 9 16:14:07 2014 -0700
    
        build: edify: use set_metadata for backuptool
    
        Also use saner permissions on backuptool.sh
    
        Change-Id: I50742b51867aa358f5924b8dc208833092a35bd9
    
    commit dac6e8fee851433ff35723f2a790e3930aaeb113
    Author: Ricardo Cerqueira <ricardo@cyngn.com>
    Date:   Wed Nov 5 18:16:43 2014 +0000
    
        ota: Include copies of the recovery scripts even if shipping in block mode
    
    commit 5a6d5b87d44f638307531f29c89d19b41c79532b
    Author: Ricardo Cerqueira <ricardo@cyngn.com>
    Date:   Wed Nov 5 19:58:59 2014 +0000
    
        ota: Fix recovery-script cleanups in block mode
    
        Change-Id: Iadb480f8062cfb7a0e6c9024e32ac0d71a7481d7
    
    Change-Id: I2401d334d8e16d18d3770b339ec2c787dd8e477c
    
     Conflicts:
    	core/Makefile
    	tools/releasetools/ota_from_target_files.py

--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -177,6 +177,10 @@ A/B OTA specific options
       ones. Should only be used if caller knows it's safe to do so (e.g. all the
       postinstall work is to dexopt apps and a data wipe will happen immediately
       after). Only meaningful when generating A/B OTAs.
+
+  --backup <boolean>
+      Enable or disable the execution of backuptool.sh.
+      Disabled by default.
 """
 
 from __future__ import print_function
@@ -234,6 +238,7 @@ OPTIONS.skip_postinstall = False
 OPTIONS.retrofit_dynamic_partitions = False
 OPTIONS.skip_compatibility_check = False
 OPTIONS.output_metadata_path = None
+OPTIONS.backuptool = False
 
 
 METADATA_NAME = 'META-INF/com/android/metadata'
@@ -895,6 +900,16 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
   script.AppendExtra("ifelse(is_mounted(\"/system\"), unmount(\"/system\"));")
   device_specific.FullOTA_InstallBegin()
 
+  CopyInstallTools(output_zip)
+  script.UnpackPackageDir("install", "/tmp/install")
+  script.SetPermissionsRecursive("/tmp/install", 0, 0, 0755, 0644, None, None)
+  script.SetPermissionsRecursive("/tmp/install/bin", 0, 0, 0755, 0755, None, None)
+
+  if OPTIONS.backuptool:
+    script.Mount("/system")
+    script.RunBackup("backup")
+    script.Unmount("/system")
+
   system_progress = 0.75
 
   if OPTIONS.wipe_user_data:
@@ -951,6 +966,13 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
       "boot.img", "boot.img", OPTIONS.input_tmp, "BOOT")
   common.CheckSize(boot_img.data, "boot.img", target_info)
   common.ZipWriteStr(output_zip, "boot.img", boot_img.data)
+
+
+  if OPTIONS.backuptool:
+    script.ShowProgress(0.02, 10)
+    script.Mount("/system")
+    script.RunBackup("restore")
+    script.Unmount("/system")
 
   script.ShowProgress(0.05, 5)
   script.WriteRawImage("/boot", "boot.img")
@@ -2176,6 +2198,8 @@ def main(argv):
       OPTIONS.skip_compatibility_check = True
     elif o == "--output_metadata_path":
       OPTIONS.output_metadata_path = a
+    elif o in ("--backup"):
+      OPTIONS.backuptool = bool(a.lower() == 'true')
     else:
       return False
     return True
@@ -2210,6 +2234,7 @@ def main(argv):
                                  "retrofit_dynamic_partitions",
                                  "skip_compatibility_check",
                                  "output_metadata_path=",
+                                 "backup=",
                              ], extra_option_handler=option_handler)
 
   if len(args) != 2:
