308e3ba70517c37d7abeb4b473aaa63303282eb0
Ricardo Cerqueira <github@cerqueira.org>
2017-08-22 23:06:24 +0200

    Preliminary support for product builds
    Change-Id: I2a5e70dda973a6fcdbba0d5e26b35b99d3f1aea2
    Signed-off-by: mracar07 <umutcan@umutcanacar.me>

--- envsetup.sh
+++ envsetup.sh
@@ -142,6 +142,14 @@ function check_product()
         echo "Couldn't locate the top of the tree.  Try setting TOP." >&2
         return
     fi
+
+    if (echo -n $1 | grep -q -e "^aosap_") ; then
+       AOSAP_BUILD=$(echo -n $1 | sed -e 's/^aosap_//g')
+    else
+       AOSAP_BUILD=
+    fi
+    export AOSAP_BUILD
+
         TARGET_PRODUCT=$1 \
         TARGET_BUILD_VARIANT= \
         TARGET_BUILD_TYPE= \
